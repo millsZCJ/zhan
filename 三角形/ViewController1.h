e6936449e3eee9aa5d067c4843db8f5b2b5c5318
湛楚健 <301096994@qq.com>
2015-12-28 14:20:13 +0800

    no message

---
+++ 三角形/ViewController1.h
@@ -0,0 +1,13 @@
+//
+//  ViewController1.h
+//  三角形
+//
+//  Created by 湛楚健 on 15/12/28.
+//  Copyright © 2015年 ZCJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController1 : UIViewController
+
+@end
---
+++ 三角形/ViewController1.m
@@ -0,0 +1,37 @@
+//
+//  ViewController1.m
+//  三角形
+//
+//  Created by 湛楚健 on 15/12/28.
+//  Copyright © 2015年 ZCJ. All rights reserved.
+//
+
+#import "ViewController1.h"
+
+@interface ViewController1 ()
+
+@end
+
+@implementation ViewController1
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
